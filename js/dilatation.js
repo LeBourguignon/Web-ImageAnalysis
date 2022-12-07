ecaec5459e4d8ca8acd387eb94eacad5126793f8
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-12-07 20:05:43 +0100

    Update dilatation.js

--- js/dilatation.js
+++ js/dilatation.js
@@ -4,16 +4,22 @@ export function dilatation() {
     const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 	const out = new ImageData(imgData.width, imgData.height);
 	for (let i = 0; i < imgData.data.length; i += 4) {
+		out.data[i] = 0;
+		out.data[i + 1] = 0;
+		out.data[i + 2] = 0;
+		out.data[i + 3] = imgData.data[i + 3];
+
 		for (let j = -radius; j < radius; j++) {
 			for (let k = -radius; k < radius; k++) {
-				if (i + j * canvas.width + k * 4 >= 0 && i + j * canvas.width + k * 4 < imgData.data.length) {
-					if (imgData.data[i + j * canvas.width + k * 4] > 0) {
-						out.data[i] = 255;
-						out.data[i + 1] = 255;
-						out.data[i + 2] = 255;
-						out.data[i + 3] = imgData.data[i + 3];
-						j = radius;
-						k = radius;
+				if ((i % canvas.width) + k * 4 >= 0 && (i % canvas.width) + k * 4 < canvas.width) {
+					if (i + j * canvas.width + k * 4 >= 0 && i + j * canvas.width + k * 4 < imgData.data.length) {
+						if (imgData.data[i + j * canvas.width + k * 4] > 0) {
+							out.data[i] = 255;
+							out.data[i + 1] = 255;
+							out.data[i + 2] = 255;
+							j = radius;
+							k = radius;
+						}
 					}
 				}
 			}
