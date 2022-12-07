89ee97d3be75bf5de8345b4f1e14016296f11ef1
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-12-07 17:16:02 +0100

    Initialisation

---
+++ index.js
@@ -0,0 +1,54 @@
+const input = document.getElementById('input');
+input.addEventListener('change', handleFiles);
+
+function handleFiles(e) {
+    const ctx = document.getElementById('canvas').getContext('2d');
+    const img = new Image;
+    img.src = URL.createObjectURL(e.target.files[0]);
+    img.onload = function() {
+        ctx.canvas.width = img.width;
+        ctx.canvas.height = img.height;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0);
+    }
+}
+
+const buttonBlackAndWhite = document.getElementById('blackAndWhite');
+buttonBlackAndWhite.addEventListener('click', blackAndWhite);
+
+function blackAndWhite() {
+    const ctx = document.getElementById('canvas').getContext('2d');
+    const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < imgData.data.length; i += 4) {
+        let count = imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2];
+        let colour = count / 3;
+    
+        imgData.data[i] = colour;       // r
+        imgData.data[i + 1] = colour;   // g
+        imgData.data[i + 2] = colour;   // b
+        //imgData.data[i + 3] = 255;    // a
+    }
+    ctx.putImageData(imgData, 0, 0);
+}
+
+const buttonThresholding = document.getElementById('thresholding');
+buttonThresholding.addEventListener('click', thresholding);
+
+function thresholding(threshold) {
+    const ctx = document.getElementById('canvas').getContext('2d');
+    const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < imgData.data.length; i += 4) {
+        let count = imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2];
+
+        let colour = 0;
+        if (count / 3 > 128) {
+            colour = 255;
+        }
+
+        imgData.data[i] = colour;       // r
+        imgData.data[i + 1] = colour;   // g
+        imgData.data[i + 2] = colour;   // b
+        //imgData.data[i + 3] = 255;    // a
+    }
+    ctx.putImageData(imgData, 0, 0);
+}
