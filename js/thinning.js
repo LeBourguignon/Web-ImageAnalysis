1d10da7cc347c8a47bbaef865cd0085c3a704bd0
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2023-01-10 22:25:21 +0100

    add thinning

--- js/thinning.js
+++ js/thinning.js
@@ -1,69 +1,85 @@
-export const L = [
+import { addition } from "./addition.js";
+import { subtraction } from "./subtraction.js";
+
+export const constL = [
     [255,   255,    255],
     [null,  255,      null],
     [0,     0,      0]
 ];
 
-export const M = [
+export const constM = [
     [255,   255,    null],
     [255,   255,    0],
     [null,  0,      0]
 ];
 
-export function thinningRot(img, structuringElement, out) {
+export function thinningRot(img, structuringElement) {
+    let out = new ImageData(img.width, img.height);
+
     for (let i = 0; i < img.data.length; i += 4) {
-        out.data[i] = img.data[i];
-		out.data[i + 1] = img.data[i + 1];
-		out.data[i + 2] = img.data[i + 2];
+        out.data[i] = 255;
+		out.data[i + 1] = 255;
+		out.data[i + 2] = 255;
 		out.data[i + 3] = img.data[i + 3];
-        
-        if (out.data[i] == structuringElement[1][1]) {
-            for (let j = -1; j < 2; j++) {
-                for (let k = -1; k < 2; k++) {
-                    if ((i % (img.width * 4)) + k * 4 >= 0 && (i % (img.width * 4)) + k * 4 < img.width * 4 && (i + j * (img.width * 4) + k * 4) >= 0 && (i + j * (img.width * 4) + k * 4) < img.data.length) {
-                        if (img.data[i] != 0 && (img.data[i + j * (img.width * 4) + k * 4] != structuringElement[j+1][k+1] || structuringElement[j+1][k+1] == null)) {
-                            out.data[i] = 0;
-                            out.data[i + 1] = 0;
-                            out.data[i + 2] = 0;
-                        }
-                        // console.log({x: (i / 4) % (img.width), y: (i / 4) / (img.width)}, img.data[i], "Dans l'image", {x: (i / 4) % (img.width) + k, y: (i / 4) / (img.width) + j}, img.data[i + j * (img.width * 4) + k * 4], structuringElement[j+1][k+1]);
+
+        for (let j = -1; j < 2; j++) {
+            for (let k = -1; k < 2; k++) {
+                if ((i % (img.width * 4)) + k * 4 >= 0 && (i % (img.width * 4)) + k * 4 < img.width * 4 && (i + j * (img.width * 4) + k * 4) >= 0 && (i + j * (img.width * 4) + k * 4) < img.data.length) {
+                    if (!(structuringElement[j+1][k+1] == img.data[i + j * (img.width * 4) + k * 4]  || structuringElement[j+1][k+1] == null)) {
+                        out.data[i] = 0;
+                        out.data[i + 1] = 0;
+                        out.data[i + 2] = 0;
                     }
-                    else {
-                        if (0 != structuringElement[j+1][k+1] || structuringElement[j+1][k+1] == null) {
-                            out.data[i] = 0;
-                            out.data[i + 1] = 0;
-                            out.data[i + 2] = 0;
-                        }
-                        // console.log({x: (i / 4) % (img.width), y: (i / 4) / (img.width)}, img.data[i], "Hors image", {x: (i / 4) % (img.width) + k, y: (i / 4) / (img.width) + j}, img.data[i + j * (img.width * 4) + k * 4], structuringElement[j+1][k+1]);
+                    // console.log(
+                    //     {x: (i / 4) % (img.width), y: (i / 4) / (img.width), valueIn: img.data[i], valueOut: out.data[i]}, 
+                    //     "Dans l'image", 
+                    //     {x: (i / 4) % (img.width) + k, y: (i / 4) / (img.width) + j, value: img.data[i + j * (img.width * 4) + k * 4]},
+                    //     structuringElement[j+1][k+1],
+                    //     structuringElement[j+1][k+1] == img.data[i + j * (img.width * 4) + k * 4],
+                    //     structuringElement[j+1][k+1] == null
+                    //     );
+                }
+                else {
+                    if (!(structuringElement[j+1][k+1] == 0 || structuringElement[j+1][k+1] == null)) {
+                        out.data[i] = 0;
+                        out.data[i + 1] = 0;
+                        out.data[i + 2] = 0;
                     }
+                    // console.log(
+                    //     {x: (i / 4) % (img.width), y: (i / 4) / (img.width), valueIn: img.data[i], valueOut: out.data[i]}, 
+                    //     "Hors image", 
+                    //     {x: (i / 4) % (img.width) + k, y: (i / 4) / (img.width) + j, value: img.data[i + j * (img.width * 4) + k * 4]}, 
+                    //     structuringElement[j+1][k+1],
+                    //     structuringElement[j+1][k+1] == img.data[i + j * (img.width * 4) + k * 4],
+                    //     structuringElement[j+1][k+1] == null
+                    //     );
                 }
             }
         }
     }
-
     return out;
 }
 
 export function thinning(img) {
-    let localL = L;
-    let localM = M;
+    let localL = constL;
+    let localM = constM;
 
-    var out = new ImageData(img.width, img.height);
+    let out = thinningRot(img, localL);
+    out = addition(out, thinningRot(img, localM));
+    localL = localL[0].map((value, index) => localL.map(row => row[row.length-1-index]));
+    localM = localM[0].map((value, index) => localM.map(row => row[row.length-1-index]));
+    out = addition(out, thinningRot(img, localL));
+    out = addition(out, thinningRot(img, localM));
+    localL = localL[0].map((value, index) => localL.map(row => row[row.length-1-index]));
+    localM = localM[0].map((value, index) => localM.map(row => row[row.length-1-index]));
+    out = addition(out, thinningRot(img, localL));
+    out = addition(out, thinningRot(img, localM));
+    localL = localL[0].map((value, index) => localL.map(row => row[row.length-1-index]));
+    localM = localM[0].map((value, index) => localM.map(row => row[row.length-1-index]));
+    out = addition(out, thinningRot(img, localL));
+    out = addition(out, thinningRot(img, localM));
 
-    out = thinningRot(img, localL, out);
-    out = thinningRot(img, localM, out);
-    localL = localL[0].map((value, index) => localL.map(row => row[row.length-1-index]));
-    localM = localM[0].map((value, index) => localM.map(row => row[row.length-1-index]));
-    out = thinningRot(img, localL, out);
-    out = thinningRot(img, localM, out);
-    localL = localL[0].map((value, index) => localL.map(row => row[row.length-1-index]));
-    localM = localM[0].map((value, index) => localM.map(row => row[row.length-1-index]));
-    out = thinningRot(img, localL, out);
-    out = thinningRot(img, localM, out);
-    localL = localL[0].map((value, index) => localL.map(row => row[row.length-1-index]));
-    localM = localM[0].map((value, index) => localM.map(row => row[row.length-1-index]));
-    out = thinningRot(img, localL, out);
-    out = thinningRot(img, localM, out);
+    out = subtraction(img, out);
 
     return out;
 }
@@ -73,5 +89,6 @@ export function showThinning() {
 	const ctx = document.getElementById('canvas').getContext('2d');
     const img = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
 	const out = thinning(img);
+    console.log(out);
     ctx.putImageData(out, 0, 0);
 }
