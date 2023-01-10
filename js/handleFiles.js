4dbdc2af796a5947b4e38505529420831a570f5f
Tom <64775524+tom-rh@users.noreply.github.com>
2023-01-10 14:04:56 +0100

    Revert "fix size canvas"
    This reverts commit c53a924cfd8b4e1651fdf4350a5a734703c09d1f.

--- js/handleFiles.js
+++ js/handleFiles.js
@@ -1,28 +1,15 @@
 let currentImage = null;
 
-function drawImageScaled(img, ctx) {
-    var canvas = ctx.canvas ;
-    var hRatio = canvas.width  / img.width    ;
-    var vRatio =  canvas.height / img.height  ;
-    var ratio  = Math.min ( hRatio, vRatio );
-    var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
-    var centerShift_y = ( canvas.height - img.height*ratio ) / 2;  
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    ctx.drawImage(img, 0,0, img.width, img.height,
-                       centerShift_x,centerShift_y,img.width*ratio, img.height*ratio);  
- }
- 
 export function handleFiles(e) {
     const ctx = document.getElementById('canvas').getContext('2d');
     const img = new Image;
     img.src = URL.createObjectURL(e.target.files[0]);
     img.onload = function() {
 		currentImage = img;
-        // ctx.canvas.width = img.width;
-        // ctx.canvas.height = img.height;
-        // ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // ctx.drawImage(img, 0, 0);
-        drawImageScaled(img, ctx);
+        ctx.canvas.width = img.width;
+        ctx.canvas.height = img.height;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0);
     }
 }
 
