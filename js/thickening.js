16677b6a079741de01bb09ca0390b6e3aa00fadf
Tom <64775524+tom-rh@users.noreply.github.com>
2023-01-11 09:02:41 +0100

    add thickening function

--- js/thickening.js
+++ js/thickening.js
@@ -1,30 +1,75 @@
-export const L = [
+import { addition } from "./addition.js";
+import { subtraction } from "./subtraction.js";
+
+export const constL = [
     [255,   255,    255],
-    [null,  1,      null],
+    [null,  0,      null],
     [0,     0,      0]
 ];
 
-export const M = [
+export const constM = [
     [255,   255,    null],
-    [255,   255,    0],
+    [255,   0,      0],
     [null,  0,      0]
 ];
 
-export function thickeningRot(img, structuringElement, out) {
-    
-    // TODO
+export function thickeningRot(img, structuringElement) {
+    let out = new ImageData(img.width, img.height);
 
+    for (let i = 0; i < img.data.length; i += 4) {
+        out.data[i] = 255;
+		out.data[i + 1] = 255;
+		out.data[i + 2] = 255;
+		out.data[i + 3] = img.data[i + 3];
+
+        for (let j = -1; j < 2; j++) {
+            for (let k = -1; k < 2; k++) {
+                if ((i % (img.width * 4)) + k * 4 >= 0 && (i % (img.width * 4)) + k * 4 < img.width * 4 && (i + j * (img.width * 4) + k * 4) >= 0 && (i + j * (img.width * 4) + k * 4) < img.data.length) {
+                    if (!(structuringElement[j+1][k+1] == img.data[i + j * (img.width * 4) + k * 4]  || structuringElement[j+1][k+1] == null)) {
+                        out.data[i] = 0;
+                        out.data[i + 1] = 0;
+                        out.data[i + 2] = 0;
+                    }
+                }
+                else {
+                    if (!(structuringElement[j+1][k+1] == 0 || structuringElement[j+1][k+1] == null)) {
+                        out.data[i] = 0;
+                        out.data[i + 1] = 0;
+                        out.data[i + 2] = 0;
+                    }
+                }
+            }
+        }
+    }
+    return out;
 }
 
-export function thickening(imgData) {
-    var out = new ImageData(imgData.width, imgData.height);
+export function thickening(img) {
+    let localL = constL;
+    let localM = constM;
 
-    // TODO
+    let out = thickeningRot(img, localL);
+    out = addition(out, thickeningRot(img, localM));
+    localL = localL[0].map((value, index) => localL.map(row => row[row.length-1-index]));
+    localM = localM[0].map((value, index) => localM.map(row => row[row.length-1-index]));
+    out = addition(out, thickeningRot(img, localL));
+    out = addition(out, thickeningRot(img, localM));
+    localL = localL[0].map((value, index) => localL.map(row => row[row.length-1-index]));
+    localM = localM[0].map((value, index) => localM.map(row => row[row.length-1-index]));
+    out = addition(out, thickeningRot(img, localL));
+    out = addition(out, thickeningRot(img, localM));
+    localL = localL[0].map((value, index) => localL.map(row => row[row.length-1-index]));
+    localM = localM[0].map((value, index) => localM.map(row => row[row.length-1-index]));
+    out = addition(out, thickeningRot(img, localL));
+    out = addition(out, thickeningRot(img, localM));
+
+    out = addition(img, out);
 
     return out;
 }
 
 export function showThickening() {
+    console.log("showThickening");
     const radius = document.getElementById("rangeRadius").value; 
 	const ctx = document.getElementById('canvas').getContext('2d');
     const imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
